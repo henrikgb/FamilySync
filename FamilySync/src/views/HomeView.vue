6bb5018cb80a8773727614707596c4137418f4a5
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2025-04-08 12:40:20 +0200

    Feat: update layout and delete redundant files

--- FamilySync/src/views/HomeView.vue
+++ FamilySync/src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+//..
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="about">
+      <h1>This is the home page</h1>
+    </div>
   </main>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
